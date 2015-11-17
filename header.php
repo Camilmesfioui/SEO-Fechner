553ae2d5cc3fdda7036c284770bea62e14e7ed0c
Camil Mesfioui <camil.mesfioui@gmail.com>
2015-11-17 09:27:50 +0100

    ajout fichiers 2

---
+++ header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head lang="fr">
+    <meta charset="UTF-8">
+    <title>Jonathan Fechner | Formateur et Consultant en Gestion de Projet</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css"/>
+    <link rel="stylesheet" href="style/animate.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- Dublin Core -->
+    <meta name="DC.title" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95">
+    <meta name="DC.creator" content="Jonathan Fechner">
+    <meta name="DC.subject" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95" />
+    <meta name="DC.description" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95" />
+    <meta name="DC.publisher" content="Jonathan Fechner">
+    <meta name="DC.format" content="website">
+    <meta name="DC.identifier" content="www.jonathanfechner.fr">
+    <meta name="DC.language" content="fr-FR">
+    <meta name="DC.coverage" content="World">
+    <meta name="DC.rights" content="&copy; Jonathan Fechner">
+    <!-- END Dublin Core -->
+
+
+    <!-- Referencement -->
+    <meta name="description" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95">
+    <meta name="keywords" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95">
+    <meta name="author" content="Jonathan Fechner">
+    <meta name="robots" content="index">
+    <meta name="Indentifier-URL" content="www.jonathanfechner.fr">
+    <!-- END Référencement -->
+
+
+    <!-- Open Graph-->
+    <meta property="og:title" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="www.jonathanfechner.fr">
+    <meta property="og:image" content="www.jonathanfechner.fr/img/logo-big.png">
+    <meta property="og:site_name" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95">
+    <meta property="og:description" content="Jonathan Fechner - Consultant SEO - Val d'Oise - 95">
+    <!-- END Open Graph-->
+
+</head>
+
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
