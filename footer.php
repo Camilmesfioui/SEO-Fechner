4333ef600cb7654be7da7ca37c3c9b3a2251cf2f
Camil Mesfioui <camil.mesfioui@gmail.com>
2015-11-17 11:47:08 +0100

    début footer

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 <footer>
-
+    <div class="container">
+        <div class="row col-centered">
+            <div class="col-md-12">
+                <p class="text-center">
+                    Jonathan Fechner, consultant et formateur T.I.C - Mentions Légales - Plan du site - Tous droits réservés ©copyright 2015
+                </p>
+            </div>
+        </div>
+    </div>
 </footer>
 
 </body>
