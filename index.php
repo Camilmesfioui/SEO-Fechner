d4e1324b8d98a9d9f764cae7f8fe408832fbb9e9
Your Name <you@example.com>
2015-11-18 10:41:49 +0100

    modifications home v2

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 
 <?php require("header.php"); ?>
+<!-- 1er over TEST
 
 <div class="container-fluid">
-	<div class="row">
-<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+	<div class="row" id="menuv">
+<div class="col-lg-3 col-md-4 col-md-offset-3 col-sm-6 col-xs-12">
+
     <div class="hovereffect">
         <img class="img-responsive" src="images/competences.jpg" alt="">
         <div class="overlay">
-           <h2>Hover effect 3</h2>
+           <h2>COMPETENCES</h2>
+           <a class="info" href="#">link here</a>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/experiences.jpg" alt="">
+        <div class="overlay">
+           <h2>EXPERIENCES</h2>
+           <a class="info" href="#">link here</a>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="row" id="menuv">
+	<div class="col-lg-3 col-md-4 col-md-offset-3 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/competences.jpg" alt="">
+        <div class="overlay">
+           <h2>TUTORIAUX</h2>
            <a class="info" href="#">link here</a>
         </div>
     </div>
@@ -17,34 +41,93 @@
     <div class="hovereffect">
         <img class="img-responsive" src="images/competences.jpg" alt="">
         <div class="overlay">
-           <h2>Hover effect 3</h2>
+           <h2>CONTACT</h2>
            <a class="info" href="#">link here</a>
         </div>
     </div>
 </div>
 </div>
+</div>
+-->
 
-<div class="row">
-	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+<!-- 2eme over TEST
+
+<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
     <div class="hovereffect">
-        <img class="img-responsive" src="images/competences.jpg" alt="">
+        <img class="img-responsive" src="http://placehold.it/350x250" alt="">
         <div class="overlay">
-           <h2>Hover effect 3</h2>
+           <h2>Hover effect 5</h2>
            <a class="info" href="#">link here</a>
         </div>
+    </div>
+</div>
+-->
+
+<div class="container-fluid homev">
+
+	<div class="row" id="menuv">
+
+<div class="col-lg-3 col-md-4 col-md-offset-3 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/competences.jpg" alt="">
+            <div class="overlay">
+                <h2>COMPETENCES</h2>
+				<p> 
+					<a href="#">En savoir plus</a>
+				</p> 
+            </div>
     </div>
 </div>
 
 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
     <div class="hovereffect">
-        <img class="img-responsive" src="images/competences.jpg" alt="">
-        <div class="overlay">
-           <h2>Hover effect 3</h2>
-           <a class="info" href="#">link here</a>
-        </div>
+        <img class="img-responsive" src="images/experiences.jpg" alt="">
+            <div class="overlay">
+                <h2>EXPERIENCES</h2>
+				<p> 
+					<a href="#">En savoir plus</a>
+				</p> 
+            </div>
     </div>
 </div>
 </div>
+
+<div class="row" id="menuv2">
+
+<div class="col-lg-3 col-md-4 col-md-offset-3 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/tutoriaux.jpg" alt="">
+            <div class="overlay">
+                <h2>TUTORIAUX</h2>
+				<p> 
+					<a href="#">En savoir plus</a>
+				</p> 
+            </div>
+    </div>
+</div>
+
+<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/contact.jpg" alt="">
+            <div class="overlay">
+                <h2>CONTACT</h2>
+				<p> 
+					<a href="#">En savoir plus</a>
+				</p> 
+            </div>
+    </div>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row" id="">
+		<div class="col-md-3 image">
+			 <img src="images/fechner.jpg">
+		</div>
+	</div>
+
+	<div class="col-md-5">
 </div>
 
 <?php require("footer.php"); ?>
