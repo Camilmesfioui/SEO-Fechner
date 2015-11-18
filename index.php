188d6758f8b563b9027d75196d4ca251a16baf37
Camil Mesfioui <camil.mesfioui@gmail.com>
2015-11-18 11:38:48 +0100

    réso bourbier

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 
 <?php require("header.php"); ?>
 
-<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-    <div class="hovereffect">
-        <img class="img-responsive" src="http://placehold.it/350x200" alt="">
-        <div class="overlay">
-           <h2>Hover effect 3</h2>
-           <a class="info" href="#">link here</a>
-        </div>
-    </div>
-</div>
+
 
 <?php require("footer.php"); ?>
 
