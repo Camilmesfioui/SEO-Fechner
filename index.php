57c6fad2dc6a9cd87d14b9639524a6882acd1fd1
Your Name <you@example.com>
2015-11-18 09:34:04 +0100

    <div class="hovereffect">         <img class="img-responsive" src="images/competences.jpg" alt="">         <div class="overlay">            <h2>Hover effect 3</h2>            <a class="info" href="#">link here</a>         </div>     </div> </div> Please enter the commit message for your changes. Lines starting

--- index.php
+++ index.php
@@ -1,14 +1,50 @@
 
 <?php require("header.php"); ?>
 
+<div class="container-fluid">
+	<div class="row">
 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
     <div class="hovereffect">
-        <img class="img-responsive" src="http://placehold.it/350x200" alt="">
+        <img class="img-responsive" src="images/competences.jpg" alt="">
         <div class="overlay">
            <h2>Hover effect 3</h2>
            <a class="info" href="#">link here</a>
         </div>
     </div>
+</div>
+
+<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/competences.jpg" alt="">
+        <div class="overlay">
+           <h2>Hover effect 3</h2>
+           <a class="info" href="#">link here</a>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/competences.jpg" alt="">
+        <div class="overlay">
+           <h2>Hover effect 3</h2>
+           <a class="info" href="#">link here</a>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="hovereffect">
+        <img class="img-responsive" src="images/competences.jpg" alt="">
+        <div class="overlay">
+           <h2>Hover effect 3</h2>
+           <a class="info" href="#">link here</a>
+        </div>
+    </div>
+</div>
+</div>
 </div>
 
 <?php require("footer.php"); ?>
