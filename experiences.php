65018d183dcc75b33b87e0ee6dc9a713bf1178ed
Arthur LEROUX <arthur_leroux@hotmail.fr>
2015-11-19 22:48:19 +0100

    retour de beul

--- experiences.php
+++ experiences.php
@@ -1,154 +1,36 @@
 <?php include ('header.php'); ?>
 
-<!--    <div id="scroller" style="width: 550px; height: 400px; margin: 0 auto;">-->
-<!--        <div class="innerScrollArea">-->
-<!--            <ul>-->
-<!--                <!-- Define photos here -->
-<!--                <li><img src="images/clients/atlantic.png" width="600" height="400" /></li>-->
-<!--                <li><img src="images/clients/axa_assurances.png" width="267" height="400" /></li>-->
-<!--                <li><img src="images/clients/banque_populaire.png" width="600" height="400" /></li>-->
-<!--                <li><img src="images/clients/dassault_systemes.png" width="535" height="400" /></li>-->
-<!--                <li><img src="images/clients/gan_assurances.png" width="267" height="400" /></li>-->
-<!--                <li><img src="images/clients/institut_de_internet_et_du_multimedia.png" width="267" height="400" /></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
 
-<article class="experiences">
-
-    <div class="container">
+    <div class="container experiences">
         <div class="row">
-            <div class="col-md-6 col-centered">
+            <div class="col-md-6 col-centered text-center">
                 <h1>Expériences professionnel</h1>
             </div>
             <div class="col-md-1 col-centered">
                 <hr class="hr-contact">
             </div>
         </div>
-    </div>
 
-
-    <div class="container">
         <div class="row">
-
-            <div class="col-md-6 col-md-offset-2">
-                <p class="right-align-border-right">
-                    Moi, Jonathan Fechner pratique depuis 15 ans la gestion de projet entant que chef de projet.
-                    J’ai travaillé comme chef de projet dans le domaine graphique, vidéo, 3D, et plus                             particulièrement, dans le domaine du digital. En effet, je suis spécialisé dans la                            stratégie digitale. Cependant, il m’est tout de même arrivé de travailler entant                              que chef de projet sur des projets de marketing digital.
-                </p>
+            <div class="col-md-4 col-md-offset-1 experience right-align-border-right">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto asperiores blanditiis consectetur delectus explicabo, laborum necessitatibus nostrum numquam pariatur quasi qui quia ratione reiciendis unde. Autem consequatur corporis quibusdam!</p>
             </div>
-
-            <div class="col-md-4">
-
-                    <ul class="List">
-                        <li class="List-item">
-                            <div class="ProgressBar ProgressBar--animateAll" data-progress="59">
-                                <svg class="ProgressBar-contentCircle">
-                                    <!-- on défini le l'angle et le centre de rotation du cercle -->
-                                    <circle transform="rotate(-90, 100, 100)" class="ProgressBar-background" cx="100" cy="100" r="95" />
-                                    <circle transform="rotate(-90, 100, 100)" class="ProgressBar-circle" cx="100" cy="100" r="95" />
-                                    <span class="ProgressBar-percentage ProgressBar-percentage--count"></span>
-                                </svg>
-                            </div>
-                        </li>
-                    </ul>
-
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-7 experience left-align-border-left">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolores earum et facere id illo in minima, modi nihil numquam obcaecati odit omnis quibusdam ratione rerum sint sunt, velit voluptatum.</p>
             </div>
-
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-1 experience right-align-border-right">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto asperiores blanditiis consectetur delectus explicabo, laborum necessitatibus nostrum numquam pariatur quasi qui quia ratione reiciendis unde. Autem consequatur corporis quibusdam!</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-7 experience left-align-border-left">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolores earum et facere id illo in minima, modi nihil numquam obcaecati odit omnis quibusdam ratione rerum sint sunt, velit voluptatum.</p>
+            </div>
         </div>
     </div>
-
-    <div class="container">
-            <div class="row">
-
-                <div class="col-md-4 col-md-offset-2">
-
-                    <ul class="List">
-                        <li class="List-item">
-                            <div class="ProgressBar ProgressBar--animateAll" data-progress="59">
-                                <svg class="ProgressBar-contentCircle">
-                                    <!-- on défini le l'angle et le centre de rotation du cercle -->
-                                    <circle transform="rotate(-90, 100, 100)" class="ProgressBar-background" cx="100" cy="100" r="95" />
-                                    <circle transform="rotate(-90, 100, 100)" class="ProgressBar-circle" cx="100" cy="100" r="95" />
-                                    <span class="ProgressBar-percentage ProgressBar-percentage--count"></span>
-                                </svg>
-                            </div>
-                        </li>
-                    </ul>
-
-                </div>
-
-
-                <div class="col-md-6">
-                    <p>
-                        Je suis aussi depuis 12 ans à la tête de F3, une entreprise du Val d’Oise                                     travaillant entant qu’agence événementiel. Cette entreprise se situe plus précisément                         à Mery sur Oise, dans le Val d’Oise, au 13 sen du moulin, code postale 95540.
-                    </p>
-                </div>
-
-            </div>
-    </div>
-
-    <div class="container">
-        <div class="row">
-
-            <div class="col-md-6">
-                <p>
-                    Je suis également un spécialiste du SEO et du référencement sur internet. Ainsi, il                           est tout à fait dans mes compétences de faire un audit de référencement et SEO d’un                           site donné. Je peux être consultant et conseiller les processeurs du site afin                                d’améliorer leurs référencement de manière naturel ou non.
-                </p>
-            </div>
-
-            <div class="col-md-6">
-
-                <ul class="List">
-                    <li class="List-item">
-                        <div class="ProgressBar ProgressBar--animateAll" data-progress="59">
-                            <svg class="ProgressBar-contentCircle">
-                                <!-- on défini le l'angle et le centre de rotation du cercle -->
-                                <circle transform="rotate(-90, 100, 100)" class="ProgressBar-background" cx="100" cy="100" r="95" />
-                                <circle transform="rotate(-90, 100, 100)" class="ProgressBar-circle" cx="100" cy="100" r="95" />
-                                <span class="ProgressBar-percentage ProgressBar-percentage--count"></span>
-                            </svg>
-                        </div>
-                    </li>
-                </ul>
-
-            </div>
-
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row">
-
-            <div class="col-md-6">
-
-                <ul class="List">
-                    <li class="List-item">
-                        <div class="ProgressBar ProgressBar--animateAll" data-progress="59">
-                            <svg class="ProgressBar-contentCircle">
-                                <!-- on défini le l'angle et le centre de rotation du cercle -->
-                                <circle transform="rotate(-90, 100, 100)" class="ProgressBar-background" cx="100" cy="100" r="95" />
-                                <circle transform="rotate(-90, 100, 100)" class="ProgressBar-circle" cx="100" cy="100" r="95" />
-                                <span class="ProgressBar-percentage ProgressBar-percentage--count"></span>
-                            </svg>
-                        </div>
-                    </li>
-                </ul>
-
-            </div>
-
-            <div class="col-md-6">
-                <p>
-                    Actuellement, j’ai aussi un statut de Consultant Indépendant Digital spécialiste des
-                    techniques de l’information et de la communication. Ce poste de consultant me permet aussi                    de dispenser nombre de formation auprès de grandes écoles tel que l’IIM (Institut de                          l’Internet et du Multimédia).
-                </p>
-            </div>
-
-        </div>
-    </div>
-
-
-
-</article>
 
 <?php include ('footer.php'); ?>
